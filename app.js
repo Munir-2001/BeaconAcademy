dc7644d5f962a45bb2d913e456caf44a2a7f7295
Munir-2001 <87448868+Munir-2001@users.noreply.github.com>
2022-08-13 15:11:09 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path= require("path");
 const app=express();
 var mongoose= require('mongoose');
 mongoose.connect('mongodb://localhost/BeaconAcademy',{useNewUrlParser:true});
-//const port = 80;
+const port = process.env.PORT || 8000;
 const bodyparser=require('body-parser')
 // define mongoose schema
 
